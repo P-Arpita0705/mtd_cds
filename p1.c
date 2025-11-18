ea292432b8ef60c5ff1f88c8eddaa0c30e8e820a
P-Arpita0705 <arpita.kavi79@gmail.com>
2025-11-18 09:21:19 +0530

    This is just to learn git

---
+++ p1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    float num = 4.75f;
+    printf("The size is: %d\n",sizeof(num));
+    printf("The size is: %d\n",sizeof(4.75f));
+    printf("The value is: %f\n",num);
+
+}
---
+++ p2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+    float num=17/3;
+    printf("Num=%d\n",num);
+    printf("Num=%f\n",num);
+}
